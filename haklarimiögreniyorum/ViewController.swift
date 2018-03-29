66d58df0c9110932e4f3e4da503f79beea67915d
Elif Tultay <eliftultay@gmail.com>
2018-03-29 20:28:34 +0300

    Initial Commit

---
+++ haklarimiögreniyorum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  haklarimiögreniyorum
+//
+//  Created by Elif Tultay on 29.03.2018.
+//  Copyright © 2018 eliftultay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
